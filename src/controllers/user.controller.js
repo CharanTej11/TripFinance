769342201bfd1d7b804f4edb52325dde0f76f7b2
CharanTej11 <charantejasampangi@gmail.com>
2025-06-16 21:32:15 +0530

    add login controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,9 @@
 import User from "../models/User.model.js";
-import bcrypt from "bcryptjs";   
+import jwt from "jsonwebtoken" 
 import asyncHandler from "../utils/asyncHandler.js";
 import  ApiError  from "../utils/ApiError.js";
 import  ApiResponse  from "../utils/ApiResponse.js";
-import hashPassword from "../utils/hashPassword.js";
+import {hashPassword,verifyPassword} from "../utils/hashPassword.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password,confirmPassword, firstName, lastName, phone } = req.body;
@@ -22,7 +22,7 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existingUser) {
     return res.status(400).json({ message: "User already exists" });
   }
-  let newPassword =await hashPassword(password);
+  let newPassword = await hashPassword(password);
   // Create new user
   const newUser = new User({
     username, email, password:newPassword, firstName, lastName, phone
@@ -39,4 +39,42 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const {  email, password } = req.body;
+  // Check if user already exists
+  if(
+    [ email, password ].some((field)=>{
+      return field?.trim() === ""
+    })
+  ){
+    throw new ApiError(400,"all fields are required")
+  }
+  let token=""
+  try {
+    const user = await User.findOne({ email });
+    if (user === null) {
+      return res.status(400).json(
+      new ApiResponse(403,{},"invalid username or password")
+     );
+    }
+    
+    if(verifyPassword(password,user.password)){
+      token = jwt.sign({
+        id:user._id
+      }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+      res.status(200).json(
+      new ApiResponse(200,{token:token},null)
+     );
+    }
+    else{
+      res.status(400).json(
+      new ApiResponse(403,{},"invalid username or password")
+     );
+    }
+  } catch (error) {
+    console.log(error)
+    throw new ApiError(500,"something went wrong while logging in ");
+  }
+})
+
+export { registerUser , loginUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { registerUser } from '../controllers/user.controller.js';
+import { registerUser,loginUser } from '../controllers/user.controller.js';
 let app = express.Router();
 
 app.get("/", (req, res) => {
     res.send("API is running...");
   })
 app.post("/register",registerUser)
+
+app.post("/login",loginUser)
 export default app;
